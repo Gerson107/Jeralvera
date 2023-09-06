c1825f7946950baa9af80e2b67c20c9ac9376a8f
Gerson107 <danimejia1995@hotmail.com>
2023-09-06 13:45:35 -0500

    update

--- src/components/socialBar/SocialBar.jsx
+++ src/components/socialBar/SocialBar.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebook, FaTwitter, FaInstagram } from 'react-icons/fa';
+import { FaFacebook, FaTwitter, FaInstagram, FaTiktok } from 'react-icons/fa';
 import './SocialBar.css'
 
 const SocialBar = () => {
@@ -11,8 +11,12 @@ const SocialBar = () => {
       <a href="https://twitter.com/vera_jeraldin" target="_blank" rel="noopener noreferrer">
         <FaTwitter />
       </a>
-      <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+      <a href="https://www.instagram.com/@jeravera" target="_blank" rel="noopener noreferrer">
         <FaInstagram />
+      </a>
+     
+      <a href="https://www.tiktok.com/@jeraldineandreave" target="_blank" rel="noopener noreferrer">
+        <FaTiktok />
       </a>
     </div>
   )
