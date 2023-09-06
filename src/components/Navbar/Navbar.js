34a16ab77cef2ac07c28caa9552b29cb54f5a471
Gerson107 <danimejia1995@hotmail.com>
2023-09-06 09:35:05 -0500

    update

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from './logo.png'
 import './navbar.css';
@@ -7,23 +7,23 @@ import './navbar.css';
 
 const Menu = () => (
   <>
-      <p><a href='#compromiso'>Compromiso</a></p>
-      <p><a href='#about'>Conoceme</a></p>
-      <p><a href='#experiencia'>Experiencia</a></p>
-      <p><a href='#news'>Noticias</a></p>
-      <p><a href='#contact'>Contacto</a></p> 
+    <p><a href='#compromiso'>Compromiso</a></p>
+    <p><a href='#about'>Conoceme</a></p>
+    <p><a href='#experiencia'>Experiencia</a></p>
+    <p><a href='#news'>Noticias</a></p>
+    <p><a href='#contact'>Contacto</a></p>
   </>
 )
 
 const Navbar = () => {
 
   const enlaceExterno = 'https://forms.gle/uyAg2xnygdbxN4x87'
-  
-  const [ togglrMenu, setToggleMenu ] = useState(false);
+
+  const [togglrMenu, setToggleMenu] = useState(false);
   const [isNavFixed, setIsNavFixed] = useState(false);
 
   const handleScroll = () => {
-    if (window.scrollY >100) { // Ajusta el valor según tu diseño
+    if (window.scrollY > 100) { // Ajusta el valor según tu diseño
       setIsNavFixed(true);
     } else {
       setIsNavFixed(false);
@@ -40,45 +40,40 @@ const Navbar = () => {
 
   return (
     <>
-    <div className='img__bg'></div>
+      <div className='img__bg'></div>
       <div className={`navigation ${isNavFixed ? 'fixed-navigation' : ''}`}>
-      <div className='gpt3__navbar-links'>
-        <div className='gpt3__navbar-links_logo'>
-         <a href="#home"><img src={logo} alt="logo"/></a> 
+        <div className='gpt3__navbar-links'>
+          <div className='gpt3__navbar-links_logo'>
+            <a href="#home"><img src={logo} alt="logo" /></a>
+          </div>
+          <div className='gpt3__navbar-links_container'>
+            <Menu />
+          </div>
         </div>
-        <div className='gpt3__navbar-links_container'>
-          <Menu/>
+        <div className='gpt3__navbar-sign'>
+          <button onClick={() => window.location.href = enlaceExterno} className='button_conta' type='button'>Contribuir</button>
+
         </div>
-      </div>
-      <div className='gpt3__navbar-sign'>
-        {/* <button className='button_contr'type='button'>Contribuir</button> */}
-        <button onClick={() => window.location.href = enlaceExterno } className='button_conta' type='button'>Contribuir</button>
-
-      </div>
-      <div className='gpt3__navbar-menu'>
-        {togglrMenu
-        ? <RiCloseLine color="#480354" size={30} onClick={() => setToggleMenu(false)}></RiCloseLine>
-        : <RiMenu3Line color="#480354" size={40} onClick={() => setToggleMenu(true)}></RiMenu3Line>
-      }
-      { togglrMenu && (
-        <div className='gpt3__navbar-menu_container scale-up-center'>
-          <div className='gpt3__navbar-menu_container-links'>
-            <Menu/>
-              <div className='gpt3__navbar-menu_container-links-sign'>
-                
-                {/* <button className='button_contr' type='button'>Contribuir</button> */}
-
-                <button className='button_conta' type='button'>Contribuir</button>
-                <div>
-
+        <div className='gpt3__navbar-menu'>
+          {togglrMenu
+            ? <RiCloseLine color="#480354" size={30} onClick={() => setToggleMenu(false)}></RiCloseLine>
+            : <RiMenu3Line color="#480354" size={40} onClick={() => setToggleMenu(true)}></RiMenu3Line>
+          }
+          {togglrMenu && (
+            <div className='gpt3__navbar-menu_container scale-up-center'>
+              <div className='gpt3__navbar-menu_container-links'>
+                <Menu />
+                <div className='gpt3__navbar-menu_container-links-sign'>
+                  <button className='button_conta' type='button' onClick={() => window.location.href = enlaceExterno}>Contribuir</button>
+                  <div>
+                  </div>
                 </div>
-               </div>
+              </div>
             </div>
-        </div>
-      )}
+          )}
 
+        </div>
       </div>
-    </div>
     </>
   )
 }
--- src/components/compromiso/Compromiso.js
+++ src/components/compromiso/Compromiso.js
@@ -9,7 +9,7 @@ const Compromiso = () => {
     <div id="compromiso" className='comprom__main'>
         <div className='comprom__main-prin'>
         <div className='comprom__main-prin_item'>
-        <VolunteerActivismIcon sx={{ fontSize: 50, color:'Black' }}/>
+        <VolunteerActivismIcon sx={{ fontSize: 50, color:'var(--color-main)' }}/>
         <h3>Lucha Anticorrupción</h3>
         <p>Continuaré mi lucha contra la corrupción, a través del control politico que permite la vigilancia y defensa de los recursos público, así como lo hice en el concejo Municipal, donde denuncié por hechos de corrupción a los miembros de la corporación.</p>
         <a href="" ><p></p>Leer mas <ArrowForwardIcon/> </a>
@@ -18,7 +18,7 @@ const Compromiso = () => {
 
 
         <div className='comprom__main-prin_item'>
-        <BeenhereIcon sx={{ fontSize: 50, color:'Black' }}/>
+        <BeenhereIcon sx={{ fontSize: 50, color:'var(--color-main)' }}/>
         <h3>Gestión y Liderazgo</h3>
         <p>Gestionaré ante el Gobierno Nacional, proyectos y recursos encaminados a fortalecer el desarrollo social y económico del departamento, ya que soy la única candidata que ha venido defendiendo las politícas públicas del gobierno del Cambio.</p>
         <a href="" ><p></p>Leer mas <ArrowForwardIcon/> </a>
@@ -26,7 +26,7 @@ const Compromiso = () => {
         </div>
 
         <div className='comprom__main-prin_item'>
-        <DynamicFormIcon sx={{ fontSize: 50, color:'black' }}/>
+        <DynamicFormIcon sx={{ fontSize: 50, color:'var(--color-main)' }}/>
         <h3>Determinación y Compromiso</h3>
         <p>Actuaré con independencia en la Asamblea Departamental aprobando los proyectos y politicas públicas que beneficien a la sociedad cesarence. Asi mismo, votare negativamente las que sean contrarias al interes general como lo hice anteriormente.</p>
         <a href="" ><p></p>Leer mas <ArrowForwardIcon/> </a>
